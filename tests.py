f8e64aacdefc3774fcfb4faca15af2a639851431
pvlvRInc <pra_dev@mail.ru>
2023-03-03 19:50:41 +0500

    Complete first task - yandex search

--- tests.py
+++ tests.py
@@ -8,3 +8,10 @@ def test_yandex_search(browser):
     yandex_main_page.enter_word("Тензор")
     suggest = yandex_main_page.check_suggest()
     assert bool(suggest)
+    yandex_main_page.press_enter()
+
+    nav_bar_elements_text = yandex_main_page.get_navigation_bar_text()
+    assert 'Поиск' and 'Картинки' in nav_bar_elements_text
+
+    links = yandex_main_page.get_links_text()
+    assert 'tensor.ru' == links[0]
